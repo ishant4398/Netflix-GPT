aeaeba822ee1d5b0ba5b596d67ff772d49ae780b
= <ishant.kushwaha@nagarro.com>
2023-11-27 16:19:33 +0530

    Header Text updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="text-3xl text-green-600">Ishant</div>;
+  return <div className="text-3xl text-green-600">Netflix</div>;
 };
 
 export default App;
