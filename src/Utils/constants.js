8cde34e27b4b76f7775682407b2447095d75bb56
ishant4398 <ishantkushwaha98@gmail.com>
2023-12-12 22:56:34 +0530

    Update Constant file

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,6 +1,9 @@
 export const PROFILE_PIC_ICON =
   "https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg";
 
+export const BACKGROUND_IMAGE_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
 const ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjUzMDhhYzBhNDFhMDYzODJhNDJkOWNhNTQyNTIyNSIsInN1YiI6IjY1Njk5ZmNhYjdkMzUyMDBhZDU2ZTU4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.214YXqCqolgOMZLcLRisPETvzYiQ3g3uXuD-PQoTq98";
 
