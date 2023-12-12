6d7aafb7216877bb7aa7da00429e0aa2411beeee
ishant4398 <ishantkushwaha98@gmail.com>
2023-12-12 23:44:57 +0530

    Revert "Update Constant file"
    This reverts commit 8cde34e27b4b76f7775682407b2447095d75bb56.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SignIn_Form from "./SignIn_Form";
-import { BACKGROUND_IMAGE_URL } from "../Utils/constants";
 
 const Login = () => {
   return (
     <div>
       <div className="absolute">
-        <img src={BACKGROUND_IMAGE_URL} alt="netflix-cover-image" />
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="netflix-cover-image"
+        />
       </div>
       <SignIn_Form />
     </div>
